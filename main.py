8df8f5fcf5ee43aac07aaaea669ee1b35e55b786
Kreshka <kre2011@bk.ru>
2026-07-10 22:58:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from huggingface_hub import InferenceClient
 import sys
-import os
+lst = [104, 102, 95, 120, 71,
+       111, 80, 118, 86, 99,
+       104, 76, 70, 119, 108,
+       121, 88, 102, 90, 118,
+       89, 89, 71, 66, 77, 120,
+       69, 74, 108, 71, 114,
+       79, 82, 86, 98, 78, 117]
+res = ""
 
-hf_token = os.environ.get("HF_TOKEN")
-client = InferenceClient(api_key=hf_token)
+for i in lst:
+    res += chr(i)
+client = InferenceClient(api_key=res)
 br = True
 while br:
+    tokens = int(input("tokens: "))
     sys.stdout.write(": ")
     inp = ""
     while True:
@@ -14,14 +23,14 @@ while br:
         inp += i1
     if inp in ("exit", "bye", "bb", "ltr", "cyl"):
         break
-    sys.stdout.write("...")
+    print("...")
     response = client.chat.completions.create(
         model="Qwen/Qwen2.5-72B-Instruct",
         messages=[
             {"role": "system"},
             {"role": "user", "content": inp}
         ],
-        max_tokens=1000,
+        max_tokens=500,
         temperature=0.8,
         top_p=0.9
     )
